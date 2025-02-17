440eecd8193bb9dd5db96eadb3bfe3f6e7b94fd5
colekntuson1 <cole.knutson1@gmail.com>
2025-02-16 18:10:39 -0600

    Cleaning out nvim

--- init.lua
+++ init.lua
@@ -1,2 +1,36 @@
-vim.wo.relativenumber = true
+vim.cmd.colorscheme("retrobox")
+vim.g.mapleader = ' '
+vim.wo.number = true
 vim.opt.swapfile = false
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+	pattern = {"*.c", "*.h"},
+	callback = function()
+		local current_file_name = vim.api.nvim_buf_get_name(0) 
+		print(current_file_name)
+--		vim.keymap.set('n', 'F5', 
+	end
+})
+-- Add this to your init.lua
+vim.keymap.set('n', '<F5>', function()
+    -- Save the current buffer
+    vim.cmd('write')
+    
+    -- Get the current file path
+    local file = vim.fn.expand('%:p')
+    local file_name = vim.fn.expand('%:t:r')
+    
+    -- Create the compilation command
+    local compile_cmd = string.format('zig cc -o %s %s', file_name, file)
+    
+    -- Create a new terminal buffer and compile
+    vim.cmd('botright new')
+    vim.fn.termopen(compile_cmd, {
+        on_exit = function(job_id, exit_code)
+            if exit_code == 0 then
+                -- If compilation successful, run the program
+                vim.fn.termopen('./' .. file_name)
+            end
+        end,
+        cwd = vim.fn.expand('%:p:h')
+    })
+end, { noremap = true, silent = true, desc = 'Compile and run C program' })
